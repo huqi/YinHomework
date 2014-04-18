ca74852ee3f69394b3fb62e26c36591ed85744e2
huqi <seven.zoro@gmail.com>
2014-04-18 11:25:55 +0800

    Fixed BezierMotion algorithm

--- HW6/HW6/BezierMotion.cpp
+++ HW6/HW6/BezierMotion.cpp
@@ -22,13 +22,13 @@ BezierMotion::~BezierMotion(void)
 
 DualQuaternion BezierMotion::calculatePir( int i, int r, double t)
 {
-	if (r==0)
+	if (r == 0)
 	{
 		return getControlPoints( i );
 	}
 	else
 	{
-		return calculatePir(i,r-1,t) + calculatePir(i+1,r-1,t);
+		return (1-t)*calculatePir(i,r-1,t) + t*calculatePir(i+1,r-1,t);
 	}
 }
 
@@ -42,14 +42,15 @@ vector<DualQuaternion> BezierMotion::curvePointsComputation()
 	vector< DualQuaternion > curve_points;
 	int degree = numberOfPositions-1;
 
-	for ( double t = 0.00; t < 1.00; t += 0.07)
+	for ( double t = 0.00; t < 1.00; t += 0.04)
 	{
 		for ( int r = 0; r <= degree; r++ )
 		{
 			for ( int i = 0; i<= degree-r; i++)
 			{
 				DualQuaternion point = calculatePir( i, r, t);
-				curve_points.push_back( point );
+				if( r == degree && i == 0 )
+					curve_points.push_back( point );
 			}
 		}
 	}
